191ead6a52c12e71d0967af3f81f31217d1967c6
Alejandro Cataño Herrera <alejandrocatanoherrera@gmail.com>
2020-08-25 15:56:44 -0500

    feat: add loctaion table and hasonethrougt relation with users

--- app/User.php
+++ app/User.php
@@ -53,4 +53,9 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Group::class);
     }
+
+    public function location() 
+    {
+        return $this->hasOneThrough(Location::class, Profile::class);
+    }
 }
