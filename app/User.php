1bae38b6c5d32910aef8fc64d0339235fd1f6705
Alejandro Cataño Herrera <alejandrocatanoherrera@gmail.com>
2020-08-25 10:48:06 -0500

    feat:add group table and pivot table for users and group in belongsToMany relations

--- app/User.php
+++ app/User.php
@@ -48,4 +48,9 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Level::class);
     }
+
+    public function groups() 
+    {
+        return $this->belongsToMany(Group::class);
+    }
 }
